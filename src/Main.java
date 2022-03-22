484de86a2901d9ffce5e48de61b197562194c06e
BassamAlim <65797540+BassamAlim@users.noreply.github.com>
2022-03-22 19:50:11 +0300

    Added Quantum to RR

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
-import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
 
-    private static final LinkedList<RRJob> jobs = new LinkedList<>();
-
     public static void main(String[] args) {
+        System.out.print("Enter Quantum Size: ");
+        Scanner sc = new Scanner(System.in);
+        int quantum = sc.nextInt();
+
+
         SRT srt = new SRT();
         Priority priority = new Priority();
-        RR rr = new RR();
+        RR rr = new RR(quantum);
 
 
     }
